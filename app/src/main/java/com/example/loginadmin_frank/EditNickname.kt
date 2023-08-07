9f3e345c90e08d1051d9012b90ccd457ea0878f8
FrankinKor <tgifrank32@gmail.com>
2023-08-07 21:32:20 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult) 이동

--- app/src/main/java/com/example/loginadmin_frank/EditNickname.kt
+++ app/src/main/java/com/example/loginadmin_frank/EditNickname.kt
@@ -1,7 +1,10 @@
 package com.example.loginadmin_frank
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.newNicknameEdt
 import kotlinx.android.synthetic.main.activity_edit_nickname.okBtn
 
 class EditNickname : AppCompatActivity() {
@@ -10,6 +13,17 @@ class EditNickname : AppCompatActivity() {
         setContentView(R.layout.activity_edit_nickname)
 
         okBtn.setOnClickListener {
+
+//            입력한 새 닉네임이 뭔지? 변수로 저장
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+//            변경된 닉네임을 가지고 메인으로 복귀
+
+//            입력한 닉네임을 담아주기 위한 용도로만 사용하는 Intent
+             val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNewNickname)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
 
 
         }
